b610e33b1ff0b9ae1b6c72b88acee1e2bf354df9
Emmanuel Mbonza <emmanuelmbonza@outlook.com>
2020-08-03 10:09:50 +0800

    Create Exo2_18.c

---
+++ Chapter 01/Exo2_18.c
@@ -0,0 +1,32 @@
+/*
+    C Program : Comparing Integers
+    Exercise : 2.18
+    Description : Write a program that asks the user to enter the length and breadth of a rectangle and obtains the numbers from the user.
+                  If the length and the breadth are equal, print the message "It is a Square"
+                  else print the message "It is not a Square".
+    Programmer : Emmanuel Mbonza Mayambi
+    Email : emmanuelmbonza@outlook.com
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int length;
+    int breadth;
+
+    printf("Enter the length : ");
+    scanf("%d", &length);
+
+    printf("Enter the breadth : ");
+    scanf("%d", &breadth);
+
+    if(length == breadth){
+        printf("It is a Square\n");
+    }
+    if(length != breadth){
+        printf("It is not a Square\n");
+    }
+
+    return 0;
+}
