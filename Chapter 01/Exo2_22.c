9ecff49731fc571ed5d783de7c55b54f0b7c0124
Emmanuel Mbonza <emmanuelmbonza@outlook.com>
2020-08-04 09:37:03 +0800

    Create Exo2_22.c

---
+++ Chapter 01/Exo2_22.c
@@ -0,0 +1,17 @@
+/*
+    C Program : Shapes with Asterisks
+    Exercise : 2.22
+    Description : What does the following code print ?
+                  printf("****\n*\n*\n*\n*\n*\n ****\n");
+    Programmer : Emmanuel Mbonza Mayambi
+    Email : emmanuelmbonza@outlook.com
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("****\n*\n*\n*\n*\n*\n ****\n");
+
+    return 0;
+}
