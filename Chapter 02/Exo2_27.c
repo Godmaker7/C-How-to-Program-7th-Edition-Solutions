2f0061413807f62b4c0f09174443a1392625b7c5
Emmanuel Mbonza <jasonrayley@gmail.com>
2022-01-23 23:17:57 +0800

    Create Exo2_27.c

---
+++ Chapter 02/Exo2_27.c
@@ -0,0 +1,35 @@
+/*
+    C Program : Pyramid Pattern of Asterisks
+    Exercise : 2.27
+    Description : Display the following pyramid pattern with five printf statments
+                  and then display the same pattern using a single printf statment.
+                                               *
+                                              ***
+                                             *****
+                                            *******
+                                           *********
+    Programmer : Emmanuel Mbonza Mayambi
+    Email : emmanuelmbonza@outlook.com
+*/
+
+#include <stdlib.h>
+
+//function main begins program execution
+int main()
+{
+
+    puts("");//print a new line
+
+    //pattern with five print statements
+    printf("\t     *\n");
+    printf("\t    ***\n");
+    printf("\t   *****\n");
+    printf("\t  *******\n");
+    printf("\t *********\n\n");
+
+    //pattern with one print statements
+    printf("\t     *\n\t    ***\n\t   *****\n\t  *******\n\t *********\n");
+
+    return 0;
+}// end of function main
+
