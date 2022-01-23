459e398e89066acc873156a36bf821c80c8e1558
Emmanuel Mbonza <jasonrayley@gmail.com>
2022-01-23 23:18:14 +0800

    Create Exo2_30.c

---
+++ Chapter 02/Exo2_30.c
@@ -0,0 +1,32 @@
+/*
+    C Program : Separating Digits in an Integer
+    Exercise : 2.30
+    Description : Write a program which inputs one five-digit number,
+                  separates the number into its individual digits, and
+                  prints the digits separated from one another by three
+                  spaces each.
+    Programmer : Emmanuel Mbonza Mayambi
+    Email : emmanuelmbonza@outlook.com
+*/
+
+#include <stdlib.h>
+
+//function main begins program execution
+int main()
+{
+
+    int integer;
+
+    printf("\n  Enter the integer : ");
+    scanf("%d", &integer);
+
+    int firstDigit = integer / 10000 % 10;
+    int secondDigit = integer / 1000 % 10;
+    int thirdDigit = integer / 100 % 10;
+    int fourthDigit = integer / 10 % 10;
+    int fifthDigit = integer % 10;
+
+    printf("\n  %d   %d   %d   %d   %d\n", firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit);
+
+    return 0;
+}//end of function main
