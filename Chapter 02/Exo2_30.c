087109547ecd63ebf781477a3ec7afe354fc6204
Emmanuel Mbonza <jasonrayley@gmail.com>
2022-02-14 15:55:08 +0800

    Update Exo2_30.c

--- Chapter 02/Exo2_30.c
+++ Chapter 02/Exo2_30.c
@@ -20,13 +20,15 @@ int main()
     printf("\n  Enter the integer : ");
     scanf("%d", &integer);
 
-    int firstDigit = integer / 10000 % 10;
-    int secondDigit = integer / 1000 % 10;
-    int thirdDigit = integer / 100 % 10;
-    int fourthDigit = integer / 10 % 10;
-    int fifthDigit = integer % 10;
+    puts("");//print a new line
 
-    printf("\n  %d   %d   %d   %d   %d\n", firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit);
+    while(integer > 0) //do till integer greater than  0
+    {
+        int mod = integer % 10;  //split last digit from number
+        printf("%d   ",mod); //print the digit.
+
+        integer = integer / 10;    //divide integer by 10.
+    }
 
     return 0;
 }//end of function main
